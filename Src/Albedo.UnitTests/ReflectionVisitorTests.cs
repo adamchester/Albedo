ba300b2ea4f74089497c89b11e4b6cfed1b86cf8
Adam Chester <adamchester@gmail.com>
2013-11-02 12:20:20 +0930

    temp

---
+++ Src/Albedo.UnitTests/ReflectionVisitorTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace Ploeh.Albedo.UnitTests
+{
+    public class ReflectionVisitorTests
+    {
+        [Fact]
+        public void SutIsIReflectionVisitorOfT()
+        {
+            // Fixture setup
+            // Exercise system
+            var sut = new TestReflectionVisitor<int>(0);
+            // Verify outcome
+            Assert.IsAssignableFrom<IReflectionVisitor<int>>(sut);
+            // Teardown
+        }
+
+        [Fact]
+        public void AllVisitOverloadsReturnThisByDefault()
+        {
+            // Fixture setup
+            var sut = new TestReflectionVisitor<int>(0);
+            // Exercise system
+            var visitMethods = sut.GetType()
+                .GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                .Where(m => m.Name == "Visit");
+            // Verify outcome
+            foreach (var visitOverload in visitMethods)
+            {
+                
+            }
+            // Teardown
+        }
+
+        class TestReflectionVisitor<T> : ReflectionVisitor<T>
+        {
+            private readonly T value;
+
+            public TestReflectionVisitor(T value)
+            {
+                this.value = value;
+            }
+
+            public override T Value { get { return this.value; } }
+        }
+    }
+}
